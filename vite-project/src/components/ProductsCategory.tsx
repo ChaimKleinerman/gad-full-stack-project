b27c6d1f96623ba3c133153e51a111f51bc82a17
meir b <marb051297@gmail.com>
2023-11-14 15:11:51 +0200

    bold

--- vite-project/src/components/ProductsCategory.tsx
+++ vite-project/src/components/ProductsCategory.tsx
@@ -16,47 +16,31 @@ export default function ProductsCategory({
 }: Props) {
   if (filterType === "brand") {
     return (
-      <Box >
-        {products.map((product) => {
-          if (product.brand === filter || filter === "") {
-            return (
-              <Stack>
-                <BasicCard key={product.id} product={product} />
-              </Stack>
-            );
-          }
-          return null; // Added to satisfy the return requirement for the map function
-        })}
+      <Box sx={{ display: "flex", width: "100%", flexWrap: "wrap" }}>
+        {products.map((product) =>
+          product.brand === filter || filter === "" ? (
+            <Stack key={product.id} sx={{ margin: "20px" }}>
+              <BasicCard product={product} />
+            </Stack>
+          ) : null
+        )}
       </Box>
     );
   } else {
     const filterSplitNumber = parseInt(filter.split("-")[1]);
     const filterSplitNumber2 = parseInt(filter.split("-")[0]);
-    console.log(filterSplitNumber);
 
     return (
-      <div>
-        {products.map((product) => {
-          console.log(Math.floor(product[filterType]));
-
-          if (
-            Math.floor(product[filterType]) <= filterSplitNumber &&
-            Math.floor(product[filterType]) > filterSplitNumber2
-          ) {
-            return (
-              <div key={product.id}>
-                <h1>{product.title}</h1>
-                <h2>{product.description}</h2>
-                <h3>{product.price}</h3>
-                <img src={product.thumbnail} id="imgCard" />
-                <h5>{product.discountPercentage}</h5>
-                <h5>{product.rating}</h5>
-              </div>
-            );
-          }
-          return null;
-        })}
-      </div>
+      <Box sx={{ display: "flex", width: "100%", flexWrap: "wrap" }}>
+        {products.map((product) =>
+          Math.floor(product[filterType]) <= filterSplitNumber &&
+          Math.floor(product[filterType]) > filterSplitNumber2 ? (
+            <Stack  key={product.id} sx={{ margin: "20px" }}>
+              <BasicCard product={product} />
+            </Stack>
+          ) : null
+        )}
+      </Box>
     );
   }
 }
