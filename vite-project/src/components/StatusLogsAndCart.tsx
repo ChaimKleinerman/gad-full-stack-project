3e2c6d587f25d4cfadd1c30e7d032681fcb82fd3
pinchassh <141713748+pinchassh@users.noreply.github.com>
2023-11-15 15:27:00 +0200

    login

--- vite-project/src/components/StatusLogsAndCart.tsx
+++ vite-project/src/components/StatusLogsAndCart.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Link, Route, useParams } from 'react-router-dom'
-import React from 'react';
-import { IconButton } from '@mui/material';
+import React, { useState } from 'react';
+import { Button, IconButton } from '@mui/material';
 // import Link from '@mui/material/Link';
 import Box from '@mui/material/Box';
 import ShoppingCartCheckoutIcon from '@mui/icons-material/ShoppingCartCheckout';
@@ -9,6 +9,12 @@ import SignUp from './mui/SignUp';
 
 const StatusLogsAndCart = () => {
     const [flag, setFlag] = React.useState(false);
+    const [storage, setStorage] = useState(false)
+    const signOut = () => {
+        localStorage.removeItem('email');
+        setStorage(prev => !prev)
+        console.log(localStorage.getItem('email'));
+    }
     return (
         <Box sx={containerStyle}>
             <Link
@@ -16,12 +22,25 @@ const StatusLogsAndCart = () => {
                 to={`/cart`}>
                 <ShoppingCartCheckoutIcon />
             </Link>
-            <Box style={textBoxStyle}>
-                Hello Guest
-                <br />
-                <SignIn setFlag = {setFlag} />
-                <SignUp flag = {flag} setFlag = {setFlag}/>
+            {/* <Box>{localStorage.getItem('email') ? */}
+            <Box>{storage ?
+                <Box style={textBoxStyle}>
+                    Hello User
+                    <br />
+                    <SignIn setFlag={setFlag} setStorage={setStorage} />
+                    <SignUp flag={flag} setFlag={setFlag} />
+                </Box>
+                :
+                <Box style={textBoxStyle}>
+                    Hello Guest
+                    <br />
+                    <Button
+                        style={iconButtonStyle}
+                        onClick={() => signOut()}
+                    >Sign out</Button>
+                </Box>}
             </Box>
+
         </Box>
     );
 };
@@ -38,6 +57,7 @@ const containerStyle = {
 
 const iconButtonStyle = {
     color: 'white',
+    cursor: 'pointer'
 };
 
 const textBoxStyle = {
--- vite-project/src/components/mui/SignIn.tsx
+++ vite-project/src/components/mui/SignIn.tsx
@@ -8,35 +8,43 @@ import DialogContentText from "@mui/material/DialogContentText";
 import DialogTitle from "@mui/material/DialogTitle";
 import { useForm } from "react-hook-form";
 import axios from "axios";
+import { Link } from "@mui/material";
 
 interface Props {
     setFlag: React.Dispatch<React.SetStateAction<boolean>>;
+    setStorage: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-export default function SignIn({ setFlag }: Props) {
+export default function SignIn({ setFlag, setStorage }: Props) {
     const { register, handleSubmit } = useForm();
     const [open, setOpen] = React.useState(false);
 
     const handleClose = async (dataLog: any) => {
-        console.log(dataLog);
-    
+        console.log('data:', dataLog);
+
         try {
             const response = await axios.post("http://localhost:3000/api/login", dataLog);
-    
+            // console.log(response, 'qwertyuytretyuiuytrew');
+
             if (response.status === 200) {
                 localStorage.setItem("email", dataLog.email);
+                setStorage(prev => !prev);
                 console.log('Login success', response.data);
                 const email = localStorage.getItem("email");
                 console.log(email);
                 setOpen(false);
             } else {
+                alert(`Unexpected response status: ${response.status} `);
                 console.error('Unexpected response status:', response.status);
             }
         } catch (error) {
+            alert(`Unexpected response status: 500`);
             console.error('Error in Axios request:', error);
         }
     };
-    
+    const handleClickClose = () => {
+        setOpen(false);
+    };
 
     const handleClickOpen = () => {
         setOpen(true);
@@ -44,11 +52,14 @@ export default function SignIn({ setFlag }: Props) {
 
     return (
         <React.Fragment>
-            <Button variant="outlined" onClick={handleClickOpen}>
+            <Button
+                sx={{ color: '#ffffff', cursor: 'pointer' }}
+
+                onClick={handleClickOpen}>
                 Sign In
             </Button>
 
-            <Dialog open={open} onClose={() => {}}>
+            <Dialog open={open} onClose={handleClickClose}>
                 <DialogTitle>Connect</DialogTitle>
                 <DialogContent>
                     <DialogContentText>
@@ -80,15 +91,21 @@ export default function SignIn({ setFlag }: Props) {
                                 onClick={() => {
                                     setOpen(false);
                                 }}
-                            >
+                                variant="outlined">
                                 Cancel
                             </Button>
-                            <Button type="submit">Sign In</Button>
+                            <Button
+                                type="submit"
+                                variant="outlined">Sign In</Button>
                         </DialogActions>
                     </form>
                 </DialogContent>
                 <DialogActions>
-                    <Button onClick={()=>setFlag(true)}>Switch to Sign Up</Button>
+                    <Button
+                        onClick={() => setFlag(true)}
+                    >
+                        To Sign Up
+                    </Button>
                 </DialogActions>
             </Dialog>
         </React.Fragment>
