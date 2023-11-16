c6a303a8d96961b83206df0bf399ee0d12221ca4
DvirBitan123 <dvirbitan123455@gmail.com>
2023-11-16 15:12:32 +0200

    client with 2 store in OL

--- vite-project/src/components/MapsComponents/MapComponent.tsx
+++ vite-project/src/components/MapsComponents/MapComponent.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "ol/ol.css";
 import Map from "ol/Map";
 import View from "ol/View";
@@ -7,96 +7,206 @@ import OSM from "ol/source/OSM";
 import { Zoom, MousePosition } from "ol/control";
 import { createStringXY } from "ol/coordinate";
 import { Feature } from "ol";
-import { Point } from "ol/geom";
+import Point from "ol/geom/Point";
 import VectorSource from "ol/source/Vector";
 import Style from "ol/style/Style";
 import VectorLayer from "ol/layer/Vector";
 import Icon from "ol/style/Icon";
 import Overlay from "ol/Overlay";
 import Popup from "./Popup";
-import iconStore from "../../assets/Pin.svg"
+import iconStore from "../../assets/Pin.svg";
 import { fromLonLat } from "ol/proj";
-    interface Props {
-          coordinate1: number;
-          coordinate2: number;
-      }
-function MapComponent({coordinate1,coordinate2}:Props) {
-    const zoomControl = new Zoom({});
-    const mousePositionControl = new MousePosition({
-        coordinateFormat: createStringXY(4),
-        projection: "EPSG:4326",
-        className: "custom-mouse-position",
-    });
-    const mapTargetElement = useRef<HTMLDivElement>(null);
-    const [map, setMap] = useState<Map | undefined>();
-    const position = useRef([0, 0]);
-    const features = useRef<Feature[]>([]);
-    const popup = useRef<Overlay | null>(null);
-    const flag = useRef<boolean>(false)
-    const popupElement = useRef<HTMLDivElement | null>(null);
-    useEffect(() => {
-        const postionStore = [coordinate1,coordinate2]
-        const map = new Map({
-            layers: [new TileLayer({ source: new OSM() })],
-            controls: [zoomControl, mousePositionControl],
-            view: new View({
-                center: [0, 0],
-                zoom: 0,
-                minZoom: 0,
-                maxZoom: 28,
-            }),
-        });
-        const vectorSource = new VectorSource({
-            features: [],
-        });
-        const vectorLayer = new VectorLayer({
-            source: vectorSource,
-            style: new Style({
-                image: new Icon({
-                    src: iconStore,
-                    width: 30,
-                    height: 30,
-                }),
-            }),
-        });
-        map.addLayer(vectorLayer);
-        map.setTarget(mapTargetElement.current || "");
-        setMap(map);
-        console.log('this postion curren',position.current)
-        console.log('this postion store',postionStore)
-            vectorSource.clear();
-            const newFeature = new Feature({
-                geometry: new Point(fromLonLat([coordinate1,coordinate2])),
-            });
-            vectorSource.addFeatures([newFeature]);
-            if (popup.current)
-            popupElement.current!.style.display = "none";
-        if(!popupElement.current)
-        return (console.log('problem'))
-        popup.current = new Overlay({
-            element: popupElement.current,
-            positioning: "bottom-center",
-            // offset: [0, -10],
-            // stopEvent: true,
-        });
-        map.addOverlay(popup.current);
-        return () => map.setTarget("");
-    }, [coordinate1,coordinate2]);
-    const rotateLeft = (map: Map|undefined) => {
-        const view = map!.getView();
-        const currentRotation = view.getRotation();
-        const newRotation = currentRotation + Math.PI / 6;
-        view.setRotation(newRotation);
-    };
-    return (
-        <>
-        <Popup popupRef = {popupElement}/>
-            <div
-                ref={mapTargetElement}
-                style={{ width: "50%", height: "300px" }}
-            >
-            </div>
-        </>
-    );
+
+interface Props {
+  coordinate1: number;
+  coordinate2: number;
+  coordinate3: number;
+  coordinate4: number;
 }
+
+function MapComponent({ coordinate1, coordinate2, coordinate3, coordinate4 }: Props) {
+  const mousePositionControl = new MousePosition({
+    coordinateFormat: createStringXY(4),
+    projection: "EPSG:4326",
+    className: "custom-mouse-position",
+  });
+  const mapTargetElement = useRef<HTMLDivElement>(null);
+  const [map, setMap] = useState<Map | undefined>();
+  const features = useRef<Feature[]>([]);
+  const popup = useRef<Overlay | null>(null);
+  const popupElement = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    const positionStore = [coordinate1, coordinate2, coordinate3, coordinate4];
+
+    const map = new Map({
+      layers: [new TileLayer({ source: new OSM() })],
+      view: new View({
+        center: [0, 0],
+        zoom: 0,
+        minZoom: 0,
+        maxZoom: 28,
+      }),
+    });
+
+    const vectorSource = new VectorSource({
+      features: [],
+    });
+
+    const vectorLayer = new VectorLayer({
+      source: vectorSource,
+      style: new Style({
+        image: new Icon({
+          src: iconStore,
+          width: 30,
+          height: 30,
+        }),
+      }),
+    });
+
+    map.addLayer(vectorLayer);
+    map.setTarget(mapTargetElement.current || "");
+    setMap(map);
+
+    vectorSource.clear();
+
+    // Add the first feature
+    const newFeature1 = new Feature({
+      geometry: new Point(fromLonLat([coordinate1, coordinate2])),
+    });
+
+    // Add the second feature
+    const newFeature2 = new Feature({
+      geometry: new Point(fromLonLat([coordinate3, coordinate4])),
+    });
+
+    vectorSource.addFeatures([newFeature1, newFeature2]);
+
+    if (popup.current) popupElement.current!.style.display = "none";
+
+    if (!popupElement.current) return console.log("Problem");
+
+    popup.current = new Overlay({
+      element: popupElement.current,
+      positioning: "bottom-center",
+    });
+
+    map.addOverlay(popup.current);
+
+    return () => map.setTarget("");
+  }, [coordinate1, coordinate2, coordinate3, coordinate4]);
+
+  // ... (other code)
+
+  return (
+    <>
+      <Popup popupRef={popupElement} />
+      <div ref={mapTargetElement} style={{ width: "50%", height: "300px" }}></div>
+    </>
+  );
+}
+
 export default MapComponent;
+
+// import { useEffect, useRef, useState } from "react";
+// import "ol/ol.css";
+// import Map from "ol/Map";
+// import View from "ol/View";
+// import TileLayer from "ol/layer/Tile";
+// import OSM from "ol/source/OSM";
+// import { Zoom, MousePosition } from "ol/control";
+// import { createStringXY } from "ol/coordinate";
+// import { Feature } from "ol";
+// import { Point } from "ol/geom";
+// import VectorSource from "ol/source/Vector";
+// import Style from "ol/style/Style";
+// import VectorLayer from "ol/layer/Vector";
+// import Icon from "ol/style/Icon";
+// import Overlay from "ol/Overlay";
+// import Popup from "./Popup";
+// import iconStore from "../../assets/Pin.svg"
+// import { fromLonLat } from "ol/proj";
+//     interface Props {
+//           coordinate1: number;
+//           coordinate2: number;
+//       }
+// function MapComponent({coordinate1,coordinate2}:Props) {
+//     const zoomControl = new Zoom({});
+//     const mousePositionControl = new MousePosition({
+//         coordinateFormat: createStringXY(4),
+//         projection: "EPSG:4326",
+//         className: "custom-mouse-position",
+//     });
+//     const mapTargetElement = useRef<HTMLDivElement>(null);
+//     const [map, setMap] = useState<Map | undefined>();
+//     const position = useRef([0, 0]);
+//     const features = useRef<Feature[]>([]);
+//     const popup = useRef<Overlay | null>(null);
+//     const flag = useRef<boolean>(false)
+//     const popupElement = useRef<HTMLDivElement | null>(null);
+//     useEffect(() => {
+//         const postionStore = [coordinate1,coordinate2]
+//         const map = new Map({
+//             layers: [new TileLayer({ source: new OSM() })],
+//             controls: [zoomControl, mousePositionControl],
+//             view: new View({
+//                 center: [0, 0],
+//                 zoom: 0,
+//                 minZoom: 0,
+//                 maxZoom: 28,
+//             }),
+//         });
+//         const vectorSource = new VectorSource({
+//             features: [],
+//         });
+//         const vectorLayer = new VectorLayer({
+//             source: vectorSource,
+//             style: new Style({
+//                 image: new Icon({
+//                     src: iconStore,
+//                     width: 30,
+//                     height: 30,
+//                 }),
+//             }),
+//         });
+//         map.addLayer(vectorLayer);
+//         map.setTarget(mapTargetElement.current || "");
+//         setMap(map);
+//         console.log('this postion curren',position.current)
+//         console.log('this postion store',postionStore)
+//             vectorSource.clear();
+//             const newFeature = new Feature({
+//                 geometry: new Point(fromLonLat([coordinate1,coordinate2])),
+//             });
+//             vectorSource.addFeatures([newFeature]);
+//             if (popup.current)
+//             popupElement.current!.style.display = "none";
+//         if(!popupElement.current)
+//         return (console.log('problem'))
+//         popup.current = new Overlay({
+//             element: popupElement.current,
+//             positioning: "bottom-center",
+//             // offset: [0, -10],
+//             // stopEvent: true,
+//         });
+//         map.addOverlay(popup.current);
+//         return () => map.setTarget("");
+//     }, [coordinate1,coordinate2]);
+//     const rotateLeft = (map: Map|undefined) => {
+//         const view = map!.getView();
+//         const currentRotation = view.getRotation();
+//         const newRotation = currentRotation + Math.PI / 6;
+//         view.setRotation(newRotation);
+//     };
+//     return (
+//         <>
+//         <Popup popupRef = {popupElement}/>
+//             <div
+//                 ref={mapTargetElement}
+//                 style={{ width: "50%", height: "300px" }}
+//             >
+//             </div>
+//         </>
+//     );
+// }
+// export default MapComponent;
--- vite-project/src/components/Product.tsx
+++ vite-project/src/components/Product.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import { BrowserRouter as Route, Link, useParams } from 'react-router-dom';
+// import { Button } from '@mui/base';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
@@ -12,9 +13,11 @@ import MapComponent from './MapsComponents/MapComponent';
 export default function ProductPage() {
     const dispatch = useAppDispatch()
     const product1 = useAppSelector((state) => state.products.product1);
+    const [coordinate1, setCoordinateSet] = useState(1)
+    const [coordinate2, setCoordinateSet2] = useState(1)
+    const [coordinate3, setCoordinateSet3] = useState(1)
+    const [coordinate4, setCoordinateSet4] = useState(1)
 
-    const [coordinate1, setCoordinateSet] = useState(1);
-    const [coordinate2, setCoordinateSet2] = useState(1);
     const { id } = useParams();
     const [product, setProduct] = useState<Product>({
         id: 1,
@@ -27,20 +30,31 @@ export default function ProductPage() {
         brand: 'string',
         category: 'string',
         thumbnail: 'string',
-        images: [''],
+        images: [],
+        coordinates:{
+            latitude: 1,
+            longitude: 1,
+          }
     });
     useEffect(() => {
         const fetchOneProduct = () => {
             fetch(`http://localhost:3000/api/products/${id}`)
                 .then(data => data.json())
                 .then((myProduct) => {
-                    setProduct(myProduct);
-                    setCoordinateSet(myProduct.coordinates.latitude);
-                    setCoordinateSet2(myProduct.coordinates.longitude);
+                    setProduct(myProduct)
+                    const foo = myProduct.coordinates.latitude;
+                    setCoordinateSet(myProduct.coordinates.latitude)
+                    setCoordinateSet2(myProduct.coordinates.longitude)
+                    setCoordinateSet3(myProduct.coordinates.latitude2)
+                    setCoordinateSet4(myProduct.coordinates.longitude2)
+                    console.log('this state',coordinate1);
+                    
+                    
+                    
                 })
         }
         fetchOneProduct()
-    }, [])
+    }, [coordinate1, coordinate2])
 
     const addToCart = async (id: string | undefined) => {
         const email = localStorage.getItem("email");
@@ -60,6 +74,7 @@ export default function ProductPage() {
         fetch(url, requestOptions)
             .then((response) => {
                 console.log(response);
+                
 
             })
             .catch((error) => {
@@ -77,26 +92,21 @@ export default function ProductPage() {
         brand,
         category,
         thumbnail,
-        images,
+        coordinates,
     } = product;
-
-    const styledText = {fontFamily: "Arial, sans-serif"};
-
     let productInfo = (
         <div>
-            <h1 style={styledText}>{title}</h1>
-            <h3 style={styledText}>{description}</h3>
-            <ul style={styledText}>
-                <li>category: {category}</li>
-                <li>brand: {brand}</li>
-                <li>Discount Percentage: {discountPercentage}%</li>
-                <li>rating: {rating}</li>
-                <li>products in stock: {stock}</li>
-            </ul>
-            <h2 style={styledText}>price: {price}$</h2>
+            <h1>{title}</h1>
+            <div>category: {category}</div>
+            <div>{description}</div>
+            <div>price: {price}</div>
+            <div>Discount Percentage: {discountPercentage}</div>
+            <div>rating: {rating}</div>
+            <div>products in stock: {stock}</div>
+            <div>brand: {brand}</div>
+            <br></br>
         </div>
     );
-
 
     const saveProductToRedux = () => {
         dispatch(saveProduct1(product));
@@ -105,28 +115,161 @@ export default function ProductPage() {
 
     return (
         <>  
-            <Link to={`/categories/${category}`} >
-                <Button variant="outlined">{`back to ${category}`}</Button>    
-            </Link>
+        <Link to={`/categories/${category}`} >
+            <Button variant="outlined">{`back to ${category}`}</Button>    
+        </Link>
+        
+        <div style={{ display: 'flex' }}>
+            <div style={{ marginRight: '80px' }}> {productInfo} </div>
+            <img src={thumbnail}></img>
+        </div>
+        <IconButton
+            onClick={() => addToCart(id)}
+            color="primary"
+               aria-label="add to shopping cart"
+        >
+            <AddShoppingCartIcon />
+        </IconButton>
+        <Link to={`/categories/${category}`} >
+            <Button variant="outlined" onClick={() => saveProductToRedux()}>compare to other product</Button>    
+        </Link>
+        {coordinate1}
+        <div><MapComponent coordinate1={coordinate1} coordinate2={coordinate2} coordinate3={4} coordinate4={5}/></div>
+        </>
+    )
+}
+
+
+
+// import { useEffect, useState } from 'react';
+// import { BrowserRouter as Route, Link, useParams } from 'react-router-dom';
+// import Button from '@mui/material/Button';
+// import IconButton from '@mui/material/IconButton';
+// import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+// import { Product } from '../typse/typse';
+// import { useAppDispatch } from '../redux/hooks';
+// import { useAppSelector } from '../redux/hooks';
+// import { saveProduct1 } from '../redux/projectsSlice';
+// import MapComponent from './MapsComponents/MapComponent';
+
+// export default function ProductPage() {
+//     const dispatch = useAppDispatch()
+//     const product1 = useAppSelector((state) => state.products.product1);
+
+//     const [coordinate1, setCoordinateSet] = useState(1);
+//     const [coordinate2, setCoordinateSet2] = useState(1);
+//     const { id } = useParams();
+//     const [product, setProduct] = useState<Product>({
+//         id: 1,
+//         title: 'The Amazing SanDisk',
+//         description: 'The amazing sandisk is really amazing.',
+//         price: 1000,
+//         discountPercentage: 1,
+//         rating: 1,
+//         stock: 1,
+//         brand: 'string',
+//         category: 'string',
+//         thumbnail: 'string',
+//         images: [''],
+//     });
+//     useEffect(() => {
+//         const fetchOneProduct = () => {
+//             fetch(`http://localhost:3000/api/products/${id}`)
+//                 .then(data => data.json())
+//                 .then((myProduct) => {
+//                     setProduct(myProduct);
+//                     setCoordinateSet(myProduct.coordinates.latitude);
+//                     setCoordinateSet2(myProduct.coordinates.longitude);
+//                 })
+//         }
+//         fetchOneProduct()
+//     }, [])
+
+//     const addToCart = async (id: string | undefined) => {
+//         const email = localStorage.getItem("email");
+
+//         const url = "http://localhost:3000/api/cart";
+//         const data = {
+//             email: email,
+//             productId: id,
+//         };
+//         const requestOptions = {
+//             method: "PUT",
+//             headers: {
+//                 "Content-Type": "application/json",
+//             },
+//             body: JSON.stringify(data),
+//         };
+//         fetch(url, requestOptions)
+//             .then((response) => {
+//                 console.log(response);
+
+//             })
+//             .catch((error) => {
+//                 console.error("Error:", error);
+//             });
+//     };
+
+//     const {
+//         title,
+//         description,
+//         price,
+//         discountPercentage,
+//         rating,
+//         stock,
+//         brand,
+//         category,
+//         thumbnail,
+//         images,
+//     } = product;
+
+//     const styledText = {fontFamily: "Arial, sans-serif"};
+
+//     let productInfo = (
+//         <div>
+//             <h1 style={styledText}>{title}</h1>
+//             <h3 style={styledText}>{description}</h3>
+//             <ul style={styledText}>
+//                 <li>category: {category}</li>
+//                 <li>brand: {brand}</li>
+//                 <li>Discount Percentage: {discountPercentage}%</li>
+//                 <li>rating: {rating}</li>
+//                 <li>products in stock: {stock}</li>
+//             </ul>
+//             <h2 style={styledText}>price: {price}$</h2>
+//         </div>
+//     );
+
+
+//     const saveProductToRedux = () => {
+//         dispatch(saveProduct1(product));
+//         console.log('redux product1:', product1);
+//     }
+
+//     return (
+//         <>  
+//             <Link to={`/categories/${category}`} >
+//                 <Button variant="outlined">{`back to ${category}`}</Button>    
+//             </Link>
             
-            <div style={{ display: 'flex' }}>
-                <div style={{ marginRight: '90px' }}> {productInfo} </div>
-                <img src={thumbnail}></img>
-            </div>
+//             <div style={{ display: 'flex' }}>
+//                 <div style={{ marginRight: '90px' }}> {productInfo} </div>
+//                 <img src={thumbnail}></img>
+//             </div>
 
             
-            <IconButton
-                onClick={() => addToCart(id)}
-                color="primary"
-                aria-label="add to shopping cart"
-            >
-                <AddShoppingCartIcon />
-            </IconButton>
-            {/* <Link to={`/categories/${category}`} ><button onClick={() => saveProductToRedux()}>compare to other product</button></Link> */}
-            <Link to={`/categories/${category}`} >
-                <Button variant="outlined" onClick={() => saveProductToRedux()}>compare to other product</Button>    
-            </Link>
-            <div><MapComponent coordinate1={coordinate1} coordinate2={coordinate2} /></div>
-        </>
-            )
-        }
+//             <IconButton
+//                 onClick={() => addToCart(id)}
+//                 color="primary"
+//                 aria-label="add to shopping cart"
+//             >
+//                 <AddShoppingCartIcon />
+//             </IconButton>
+//             {/* <Link to={`/categories/${category}`} ><button onClick={() => saveProductToRedux()}>compare to other product</button></Link> */}
+//             <Link to={`/categories/${category}`} >
+//                 <Button variant="outlined" onClick={() => saveProductToRedux()}>compare to other product</Button>    
+//             </Link>
+//             <div><MapComponent coordinate1={coordinate1} coordinate2={coordinate2} /></div>
+//         </>
+//             )
+//         }
